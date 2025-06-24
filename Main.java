84c2630e4afa948ceb447aa8807f2c87c24ee642
kyru021 <kylerubia021@gmail.com>
2025-06-24 11:26:37 +0800

    main

--- Main.java
+++ Main.java
@@ -12,17 +12,6 @@ public class Main {
         MoveManager moveManager = new MoveManager(scan);
         moveManager.loadDefaultMoves();
 
-<<<<<<< HEAD
-=======
-
-        // Main Menu
-        System.out.println("========================================================================");
-        System.out.println("| 1. Add Pokemon            4. Add Move            7. View Items       |");
-        System.out.println("| 2. View All Pokemon       5. View Moves          8. Search Items     |");
-        System.out.println("| 3. Search Pokemon         6. Search Moves        9. Exit             |");
-        System.out.println("========================================================================");
-
->>>>>>> 477d851b631eeae4a045ff5a6aafc7670eb85989
         while (true) {
             // Main Menu
             System.out.printf("\n%45s%n", "Pokémon Database Menu");
